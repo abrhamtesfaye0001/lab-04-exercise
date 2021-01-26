779f4f65784e7bbd833331ba2057cc8e6d901a31
Abrham Tesfaye <se.abrham.tesfaye@gmail.com>
2021-01-26 18:14:29 +0300

    added max,min and average to the prompt calculator

--- assets/js/promptCalculator.js
+++ assets/js/promptCalculator.js
@@ -6,7 +6,7 @@ let add = function(){
         sum+=parseInt(element);
     });
     
-    console.log(sum);
+    alert(sum);
 }
 let substract = function(){
     nums = prompt("enter numbers to substract with , delimiter");
@@ -15,10 +15,10 @@ let substract = function(){
         if(index!=0){
             output-=parseInt(element);
         }
-        
+     
     });
     
-    console.log(output);
+    alert(output);
 }
 let multiply = function(){
     nums = prompt("enter numbers to multiply with , delimiter");
@@ -27,7 +27,7 @@ let multiply = function(){
         output*=parseInt(element);
     });
     
-    console.log(output);
+    alert(output);
 }
 let divide = function(){
     nums = prompt("enter numbers to divide with , delimiter");
@@ -40,7 +40,7 @@ let divide = function(){
                 
             }
             else{
-                console.log("zero Error");
+                alert("zero Error");
                 err = 1;
                 return
             }
@@ -51,7 +51,60 @@ let divide = function(){
     if(err==1){
         return
     }
-    console.log(output);
+    alert(output);
+}
+
+
+let max = function() {
+    let numsArray = prompt("enter numbers with , delimiter").split(',');
+    let nums = numsArray.map(function (element) {
+        
+        return parseFloat(element)
+    });
+    
+    let maxNum = nums[0];
+    for(let i=0;i<nums.length;i++){
+        if (maxNum<nums[i]){
+            maxNum=nums[i];
+        }
+    }
+    alert(`your max is ${maxNum}`);
+}
+
+let min = function() {
+    let numsArray = prompt("enter numbers with , delimiter").split(',');
+    let nums = numsArray.map(function (element) {
+        
+        return parseFloat(element)
+    });
+    
+    let minNum = nums[0];
+    for(let i=0;i<nums.length;i++){
+        if (minNum>nums[i]){
+            minNum=nums[i];
+        }
+    }
+    alert(`your min is ${minNum}`);
+}
+
+let average = function() {
+    let numsArray = prompt("enter numbers with , delimiter").split(',');
+    let nums = numsArray.map(function (element) {
+        
+        return parseFloat(element)
+    });
+    
+    let sum = 0;
+    for(let i=0;i<nums.length;i++){
+        sum+=nums[i];
+    }
+    if(nums.length==0){
+        alert("empty prompt")
+    }
+    else{
+        let averageNum = sum/nums.length;
+        alert(`your min is ${averageNum}`);
+    }
 }
 
 iife = (function(){
@@ -61,28 +114,54 @@ iife = (function(){
     // divide();
     
     while(true){
-        p = prompt("what do u want to do\n 1. add\n 2. substract\n 3.multiply \n 4. divide \n Q/q. to exit")
+        p = prompt("what do u want to do\n 1. add\n 2. substract\n 3.multiply \n 4. divide \n 5. max \n 6. min\n 7. average\n Q/q. to exit")
 
         if(p=="q"||p=="Q"){
             break;
         }else{
-            if(p=="1"){
-                add();
-            }
-            else{
-                if(p=="2"){
-                    substract();
-                }
-                else{
-                    if(p=="3"){
-                        multiply();
-                    }
-                    else{
-                        if(p=="4"){
-                            divide();
-                        }
-                    }
-                }
+            // if(p=="1"){
+            //     add();
+            // }
+            // else{
+            //     if(p=="2"){
+            //         substract();
+            //     }
+            //     else{
+            //         if(p=="3"){
+            //             multiply();
+            //         }
+            //         else{
+            //             if(p=="4"){
+            //                 divide();
+            //             }
+            //         }
+            //     }
+            // }
+            switch (p) {
+                case "1":
+                    add()
+                    break;
+                case "2":
+                    substract()
+                    break;
+                case "3":
+                    multiply()
+                    break;
+                case "4":
+                    divide()
+                    break;
+                case "5":
+                    max()
+                    break;
+                case "6":
+                    min()
+                    break;
+                case "7":
+                    average()
+                    break;
+                    
+                default:
+                    break;
             }
         }
         
